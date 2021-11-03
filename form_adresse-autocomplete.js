16a42f5c0973356b34ff9e39d062c54bea2184bd
Elie-E <Elie-eymerich@protonmail.com>
2021-11-03 14:31:04 +0100

    log

--- form_adresse-autocomplete.js
+++ form_adresse-autocomplete.js
@@ -39,8 +39,8 @@ function fillInAddress() {
   // place.address_components are google.maps.GeocoderAddressComponent objects
   // which are documented at http://goo.gle/3l5i5Mr
   for (const component of place.address_components) {
+    console.log(component);
     const componentType = component.types[0];
-console.log(component);
     switch (componentType) {
       case "street_number": {
         address1 = `${component.long_name} ${address1}`;
