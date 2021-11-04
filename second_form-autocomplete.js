9ca39f253463083691bdafc9bae477443fdede13
Elie-E <Elie-eymerich@protonmail.com>
2021-11-04 17:04:36 +0100

    z

--- second_form-autocomplete.js
+++ second_form-autocomplete.js
@@ -1,30 +1,19 @@
-console.log('z');
-// $(document).ready(function(){
-//     let countrySelectedIso = $('#pays_user_adr option:selected').val().toLowerCase();
-// // Select all the "select" which id's end with "pays_user_adr" :
-//     $("select[id$='pays_user_adr']").on('change', function() {        
-//         let countrySelectedIso = $(this).val().toLowerCase();
-//    //     console.log(countrySelectedIso);
-//     });
-// });
-
-let countrySelector = document.querySelector("select[id$='pays_user_adr']");
-let countrySelectedIso = (countrySelector.options[countrySelector.selectedIndex].value).toLowerCase();
-
-console.log(countrySelectedIso);
-
-countrySelector.addEventListener('change', function(){
-    let countrySelectedIso = this.options[this.selectedIndex].value.toLowerCase();
-    console.log(countrySelectedIso);
-})
-
 let autocompleteField;
 let adresseField1;
 let adresseField2;
 let postalCodeField;
 
-window.initAutocomplete = function() {
-    console.log('zz');
+function initAutocomplete() {
+    let countrySelector = document.querySelector("select[id$='pays_user_adr']");
+    let countrySelectedIso = (countrySelector.options[countrySelector.selectedIndex].value).toLowerCase();
+
+    console.log(countrySelectedIso);
+
+    countrySelector.addEventListener('change', function(){
+    let countrySelectedIso = this.options[this.selectedIndex].value.toLowerCase();
+    initAutocomplete();
+    })
+
     adresseField1 = document.querySelector('.autocomplete_adresse_field');
     adresseField2 = document.querySelector("input[id$='adr2_user_adr']");
     postalCodeField = document.querySelector("input[id$='cp_user_adr']");
@@ -34,6 +23,9 @@ window.initAutocomplete = function() {
         fields: ["address_components", "geometry"],
         types: ["address"],
     });
+    
+    console.log(autocompleteField);
+
     adresseField1.focus();
 
     autocompleteField.addListener("place_changed", fillInAddress);}
@@ -75,9 +67,6 @@ console.log(place);
         //     document.querySelector("#state").value = component.short_name;
         //     break;
         //   }
-        //   case "country":
-        //     document.querySelector("#country").value = component.long_name;
-        //     break;
         }
       }
     
@@ -86,4 +75,4 @@ console.log(place);
       adresseField2.focus();
 }
 
-window.initAutocomplete();
+// window.initAutocomplete();
