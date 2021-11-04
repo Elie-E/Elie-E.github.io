b7d735101a0a714c6ef1921e1e02c8f401de469c
Elie-E <Elie-eymerich@protonmail.com>
2021-11-04 11:12:23 +0100

    adding js

---
+++ second_form-autocomplete.js
@@ -0,0 +1,48 @@
+console.log('z');
+// $(document).ready(function(){
+//     let countrySelectedIso = $('#pays_user_adr option:selected').val().toLowerCase();
+// // Select all the "select" which id's end with "pays_user_adr" :
+//     $("select[id$='pays_user_adr']").on('change', function() {        
+//         let countrySelectedIso = $(this).val().toLowerCase();
+//    //     console.log(countrySelectedIso);
+//     });
+// });
+
+let countrySelector = document.querySelector("select[id$='pays_user_adr']");
+let countrySelectedIso = (countrySelector.options[countrySelector.selectedIndex].value).toLowerCase();
+
+console.log(countrySelectedIso);
+
+countrySelector.addEventListener('change', function(){
+    let countrySelectedIso = this.options[this.selectedIndex].value.toLowerCase();
+    console.log(countrySelectedIso);
+})
+
+let autocompleteField;
+let adresseField1;
+let adresseField2;
+let postalCodeField;
+
+function initAutoComplete(){
+    console.log('zizi');
+    adresseField1 = document.querySelector('.autocomplete_adresse_field');
+    adresseField2 = document.querySelector("input[id$='adr2_user_adr']");
+    postalCodeField = document.querySelector("input[id$='cp_user_adr']");
+console.log(adresseField1);
+console.log(autocompleteField);
+    autocompleteField = new google.maps.places.Autocomplete(adresseField1, {
+        componentRestrictions: { country: "fr" },
+        fields: ["address_components", "geometry"],
+        types: ["address"],
+    });
+    adresseField1.focus();
+
+    autocompleteField.addEventListener("place_changed", fillInAddress);
+}
+
+function fillInAddress() {
+    const place = autocompleteField.getPlace();
+    let adresse1 = "";
+    let postalCode = "";
+    
+}
