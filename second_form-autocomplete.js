5e39af69c3404f3b402b4f6034c691cefb44deef
Elie-E <Elie-eymerich@protonmail.com>
2021-11-04 11:58:59 +0100

    z

--- second_form-autocomplete.js
+++ second_form-autocomplete.js
@@ -36,14 +36,52 @@ window.initAutocomplete = function() {
     });
     adresseField1.focus();
 
-    autocompleteField.addListener("click", fillInAddress);
-}
+    autocompleteField.addListener("place_changed", fillInAddress);}
 
 function fillInAddress() {
     console.log('zzz');
     const place = autocompleteField.getPlace();
     let adresse1 = "";
     let postalCode = "";
+
+    for (const component of place.address_components) {
+        const componentType = component.types[0];
+        switch (componentType) {
+          case "street_number": {
+            address1 = `${component.long_name} ${address1}`;
+            break;
+          }
+    
+          case "route": {
+            address1 += component.short_name;
+            break;
+          }
+    
+          case "postal_code": {
+            postcode = `${component.long_name}${postcode}`;
+            break;
+          }
+    
+          case "postal_code_suffix": {
+            postcode = `${postcode}-${component.long_name}`;
+            break;
+          }
+          case "locality":
+            document.querySelector("#locality").value = component.long_name;
+            break;
+          case "administrative_area_level_1": {
+            document.querySelector("#state").value = component.short_name;
+            break;
+          }
+          case "country":
+            document.querySelector("#country").value = component.long_name;
+            break;
+        }
+      }
+    
+      address1Field.value = address1;
+      postalField.value = postcode;
+      address2Field.focus();
 }
 
 window.initAutocomplete();
